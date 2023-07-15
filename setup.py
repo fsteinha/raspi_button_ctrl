98e86dfa438631b9daeeb6464e9e64ecb8e0a7fb
fsteinha <franksteinhans@yahoo.com>
2023-07-15 16:24:59 +0200

    Add the field for a python lib

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='raspi_button_ctrl',
+    version='1.0.0',
+    description='library with excecutable command for raspberry pi with execute system commands regarding to button pressing time',
+    author='Frank Steinhans',
+    author_email='franksteinhand@yahoo.com',
+    packages=['raspi_button_ctrl'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux :: Rasberrian',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
